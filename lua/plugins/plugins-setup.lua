686064923b2b608a40d174d7b8b0543add0e1342
yang <yang@163.com>
2023-10-04 13:59:40 +0800

    [core]: noice 命令行插件

--- lua/plugins/plugins-setup.lua
+++ lua/plugins/plugins-setup.lua
@@ -72,6 +72,19 @@ return require("packer").startup(function(use)
         run = 'make'
     }
 
+    -- 隐藏命令行
+    use({
+        "folke/noice.nvim",
+        requires = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        }
+    })
+
     -- git 相关
     use 'lewis6991/gitsigns.nvim'
     use 'sindrets/diffview.nvim'
